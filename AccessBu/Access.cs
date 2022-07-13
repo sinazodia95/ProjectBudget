0eea9f569c1417841c5dd2c828810820b8e565c4
Sinazo Ndovela <sinazodia95@gmail.com>
2022-07-13 12:26:41 +0200

    Add project files.

---
+++ AccessBu/Access.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace AccessBu
+{
+    public class Access
+    {
+        public static void AddExpense(int rent, int food, int toilet, int gym, int trans, int inter, int clothes, int family, int Electricity, int tot, string connectionString)
+        {
+            //sqlconnection con = new sqlconnection();
+            var RowsAffected = 0;
+            
+            {
+                // Create SQL connection object in using block for automatic closing and disposing
+                using (SqlConnection cnn = new SqlConnection(connectionString))
+                {
+                    // Create command object in using block for automatic disposal
+                    using (SqlCommand cmd = new SqlCommand("Masterinsertupdatedelete", cnn))
+                    {
+
+
+
+                        cmd.Parameters.Add(new SqlParameter("@rent", rent));
+                        cmd.Parameters.Add(new SqlParameter("@food", food));
+                        cmd.Parameters.Add(new SqlParameter("@toilet", toilet));
+                        cmd.Parameters.Add(new SqlParameter("@gym", gym));
+                        cmd.Parameters.Add(new SqlParameter("@trans", trans));
+                        cmd.Parameters.Add(new SqlParameter("@Inter", inter));
+                        cmd.Parameters.Add(new SqlParameter("@clothes", clothes));
+                        cmd.Parameters.Add(new SqlParameter("@family", family));
+                        cmd.Parameters.Add(new SqlParameter("@Electricity", Electricity));
+                        cmd.Parameters.Add(new SqlParameter("@Total", tot));
+                        cmd.Parameters.Add(new SqlParameter("@StatementType", "insert"));
+                        
+                        // Set CommandType
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        // Open the connection
+                        cnn.Open();
+
+                        // Execute the INSERT statement
+                        cmd.ExecuteNonQuery();
+
+
+
+
+                      var  ResultText = "Rows Affected: " + RowsAffected.ToString();
+                    }
+                }
+               
+            }
+
+        }
+    }
+}
---
+++ ProjectBudget/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using AccessBu;
+namespace ProjectBudget
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connect = "Server=LAPTOP-L4RD8ILN; Database=MonthlyBudget;Integrated Security = True;";
+            int rent, food, toilet, gym, trans, clothes, family, Electricity, Inter;
+
+            
+
+            if (!Console.IsInputRedirected)
+            {
+                Console.WriteLine("Please Enter The Fields Required");
+                Console.WriteLine("Please Enter Your rent fee");
+                rent = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please Enter Your food fee");
+                food = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please Enter Your toiletry's price");
+                toilet = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please Enter Your gym fee");
+                gym = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please Enter your transport fee");
+                trans = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please Enter you Intertainment Fee");
+                Inter = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please Enter your clothes fee");
+                clothes = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please Enter your family Allowence");
+                family = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please Enter you Electricity Fee");
+                Electricity = Convert.ToInt32(Console.ReadLine());
+
+
+                int totalExp = rent + clothes + Electricity + family + food + gym + toilet + trans + Inter;
+                Access.AddExpense(rent, food, toilet, gym, trans, Inter, clothes, family, Electricity, totalExp, connect);
+               
+                Console.WriteLine($"Your Monthly expenses are {totalExp}");
+            }
+
+
+        }
+    }
+}
